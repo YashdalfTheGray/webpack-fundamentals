30201ca75e2efdab0660b0b02a048900d7b0cbc6
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-02-13 19:33:44 -0800

    configured webpack to serve the right things at the right time

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
+const path = require('path');
+
 module.exports = {
+    context: path.resolve('js'),
     entry: ['./app.js', './utils.js'],
     output: {
+        path: path.resolve('build/js'),
+        publicPath: '/public/assets/js',
         filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: 'public'
     },
     module: {
         rules: [
